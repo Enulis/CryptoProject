991f402c07a259195f7c9fa7a3bbf6b63fb5b2df
hrvoje1212 <hrvoje1212@gmail.com>
2013-12-02 04:39:08 +0100

    added message input and cipher display

--- cryptoMania/src/main/java/com/cryptoMania/MainActivity.java
+++ cryptoMania/src/main/java/com/cryptoMania/MainActivity.java
@@ -1,16 +1,17 @@
 package com.cryptoMania;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.security.SecureRandom;
 
@@ -21,14 +22,22 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class MainActivity extends ActionBarActivity {
 
+    private String seed = "ASDFWERWFASD";
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String seed = "ASDFWERWFASD";
-        String message = "bok ja sam mali Z";
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
 
+    public byte[] encrypt(String message){
         try {
             byte[] rawKey = getRawKey(seed.getBytes());
 
@@ -39,17 +48,12 @@ public class MainActivity extends ActionBarActivity {
             byte[] encrypted = cipher.doFinal(message.getBytes());
 
             Log.e("CIPHER", encrypted.toString());
+            return encrypted;
         }catch (Exception e){
             e.printStackTrace();
-        }
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+            return new byte[0];
         }
     }
-
 
     private static byte[] getRawKey(byte[] seed) throws Exception {
         KeyGenerator kgen = KeyGenerator.getInstance("AES");
@@ -85,7 +89,7 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public class PlaceholderFragment extends Fragment {
 
         public PlaceholderFragment() {
         }
@@ -94,8 +98,34 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            Button cipherButton = (Button) rootView.findViewById(R.id.cipher_button);
+            final EditText input = (EditText) rootView.findViewById(R.id.text_input);
+            textView = (TextView) rootView.findViewById(R.id.text_view);
+
+            cipherButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    byte[] encrypted = encrypt(input.getText().toString());
+                    textView.setText(bytesToHex(encrypted));
+                }
+            });
+
             return rootView;
         }
+    }
+
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        int v;
+        for ( int j = 0; j < bytes.length; j++ ) {
+            v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 
 }
